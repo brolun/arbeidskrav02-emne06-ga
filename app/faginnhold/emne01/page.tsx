6df922fcad69eaf1737b5d707c48179c7ac187ce
Roman <roman.mitwicki@gokstadakademiet.no>
2025-09-23 12:22:02 +0200

    lagt til function

--- app/faginnhold/emne01/page.tsx
+++ app/faginnhold/emne01/page.tsx
@@ -9,3 +9,9 @@ export default function Page() {
 }
 
 type User = {id: number; name: string; role?: "admin" | "editor"  | "viewer" };
+
+function pickBy<T extends object, K extends keyof T>(obj : T, keys : K []): Pick<T, K> {
+  const out = {} as Pick<T, K>;
+  for (const k of keys) (out as any) [k] = obj[k];
+  return out;
+}
